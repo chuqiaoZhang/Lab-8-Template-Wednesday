56b3f5450db2bad0829d8cd443d2ecd38db360ad
Chuqiao <chuqiao@ualberta.ca>
2020-03-13 15:17:56 -0600

    add a failed test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -93,4 +93,11 @@ class CityListTest {
 
         assertEquals(1, cityList.countCities());
     }
+
+    @Test
+    void testCountDistinctCities() {
+        CityList cityList = mockCityList();
+
+        assertEquals(1, cityList.countDistinctCities());
+    }
 }
